3a506c3e41de623cc3a740445a9ade71f07b7f62
UpstairsForest <nikita.batalov@uzh.ch>
2023-04-18 05:32:14 +0200

    Update response fetching

--- scripts/content/api.js
+++ scripts/content/api.js
@@ -1,21 +1,29 @@
 async function processMessage(message, options) {
-  const mextensionUrl = "http://localhost:8080/messages";
+  const mextensionUrl = "https://billowing-sea-9553.cosmonic.app/messages";
   const mextensionApiConfig = {
     method: "POST",
     headers: {
-      "Content-Type": "application/json"
-    }
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*"
+    },
+    mode: "cors",
   };
 
   return fetch(mextensionUrl, {
     ...mextensionApiConfig,
-    body: JSON.stringify({ body: { body: message, user_name: "test user" }, method: options["method"] })
-  }).then((response) => {
-    return response.json();
-  }).then(body => {
-    return JSON.stringify(body["message"]);
-  }).catch((e) => {
-    console.warn(e);
-    return "could not establish a connection";
-  });
+    body: JSON.stringify({
+      body: { body: message, user_name: "test user" },
+      method: options["method"],
+    }),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((response) => {
+      return response["result"];
+    })
+    .catch((e) => {
+      console.warn(e);
+      return "could not establish a connection";
+    });
 }
